0977811ed9e800c974397c9472f05727e94675f9
Sai Charan <charankuppa@gmail.com>
2021-05-05 16:28:23 +0200

    Add city weather api

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const axios = require('axios');
+
+var cities = require("./cities");
+
+const apiKey = process.env.WEATHER_API_KEY;
+
+const getRandomCity = () => {
+    const randomCityIndex = Math.floor(Math.random() * cities.length);
+    return cities[randomCityIndex];
+}
+
+async function getWeatherData() {
+    const baseUrl = process.env.WEATHER_URL;
+    const city = getRandomCity();
+    const longitude = city.lng;
+    const latitude = city.lat;
+
+    const response = await axios.get(baseUrl + '?lat='+ latitude + '&lon=' + longitude +'&appid=' + apiKey + '&units=metric');
+    return response.data;
+}
+
+app.get("/city", (req, res, next) => {
+    getWeatherData().then((result) => res.json(result));
+});
+
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
